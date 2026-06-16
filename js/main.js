11ceb6466344aa72b88aac8fbebc4464576702e2
Sebastian Boga <sebastian.boga@outlook.com>
2026-06-16 14:13:10 +0300

    HTML, JS, CSS cleanup
    index.html:
    - Remove duplicate <title> and OG meta tags
    - Add lang='ro', <main> landmark
    - Fix <a><h1> nesting to <h1><a>
    
    main.js:
    - Remove dead localhost branch (both same URL)
    - Use fetch().json() instead of .text() + JSON.parse
    - Build search URL from current params in search()
    - Fix implicit globals (i, rezno)
    - Handle null start in pagination, fix page-1 skip
    - Stop getRandom interval on SERP pages
    - Escape job data to prevent XSS
    
    main.css:
    - Remove ~400 lines of dead .choices-* CSS
    - Remove duplicate html and .results blocks
    - Remove obsolete -ms- vendor prefixes
    - Fix duplicate width: 100%

--- js/main.js
+++ js/main.js
@@ -1,58 +1,58 @@
-var serverurl;
-if (window.location.hostname === 'localhost') {
-  serverurl = 'https://api.peviitor.ro';
-} else {
-  serverurl = 'https://api.peviitor.ro';
-}
-
+var serverurl = 'https://api.peviitor.ro';
 var pages;
 var item_html = "";
 
-var qs = new URLSearchParams(window.location.search);
-var q_s = qs.toString();
-var url = serverurl + '/v1/search/?' + q_s;
-
-function search() {
-  fetch(url)
-    .then(function(response) {
-      response.text().then(function(text) {
-        var ser_message = JSON.parse(text);
-        var results_nr = ser_message.response.numFound;
-
-        pages = Math.floor(results_nr / 10) + 1;
-        if (typeof pages !== 'undefined') {
-          showPages(pages);
-        }
-
-        var docs = ser_message.response.docs;
-        var rezno = "";
-        if (results_nr === 0) {
-          rezno = "nici un rezultat";
-        } else {
-          rezno = results_nr + " rezultate, " + pages + " pagini";
-        }
-        document.getElementById("resultsno").innerHTML = rezno;
-
-        item_html = "";
-        docs.forEach(showResults);
-        document.getElementById("results").innerHTML = item_html;
-      });
-    })
-    .catch(err => console.log(err));
+function escapeHtml(str) {
+  if (!str) return '';
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
 }
 
-function showResults(item, index) {
+function search() {
+  var qs = new URLSearchParams(window.location.search);
+  var url = serverurl + '/v1/search/?' + qs.toString();
+
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(ser_message) {
+      var results_nr = ser_message.response.numFound;
+
+      pages = Math.floor(results_nr / 10) + 1;
+      if (typeof pages !== 'undefined') {
+        showPages(pages);
+      }
+
+      var docs = ser_message.response.docs;
+      var rezno = "";
+      if (results_nr === 0) {
+        rezno = "nici un rezultat";
+      } else {
+        rezno = results_nr + " rezultate, " + pages + " pagini";
+      }
+      document.getElementById("resultsno").innerHTML = rezno;
+
+      item_html = "";
+      docs.forEach(showResults);
+      document.getElementById("results").innerHTML = item_html;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+function showResults(item) {
   item_html = item_html + "<li><p>";
-  item_html = item_html + "<label class='job_title'>" + "<a href='" + item.url + "' target=_blank>" + item.title + "</a>" + "</label>";
-  item_html = item_html + "<label class='company'>" + item.company + "</label>";
+  item_html = item_html + "<label class='job_title'><a href='" + escapeHtml(item.url) + "' target=_blank>" + escapeHtml(item.title) + "</a></label>";
+  item_html = item_html + "<label class='company'>" + escapeHtml(item.company) + "</label>";
   item_html = item_html + "<label class='location'>";
 
   if (item.remote) {
-    item_html = item_html + item.remote + " ";
+    item_html = item_html + escapeHtml(item.remote) + " ";
   }
 
   var loc = item.location || item.city || [];
-  item_html = item_html + loc.join(', ');
+  item_html = item_html + loc.map(escapeHtml).join(', ');
 
   item_html = item_html + " </label>";
   item_html = item_html + "</p></li>";
@@ -82,18 +82,21 @@ function addclassupsearch(id, classs) {
 function total(xurl) {
   fetch(xurl)
     .then(function(response) {
-      response.text().then(function(text) {
-        var ser_message = JSON.parse(text);
-        var total = ser_message.response.numFound;
-        if (total === 0) {
-          rezno = "Nu avem nici o oportunitate momentan";
-        } else {
-          rezno = "Avem " + total + " oportunități";
-        }
-        document.getElementById("resultsno").innerHTML = rezno;
-      });
+      return response.json();
     })
-    .catch((err) => console.log(err));
+    .then(function(ser_message) {
+      var total = ser_message.response.numFound;
+      var rezno;
+      if (total === 0) {
+        rezno = "Nu avem nici o oportunitate momentan";
+      } else {
+        rezno = "Avem " + total + " oportunități";
+      }
+      document.getElementById("resultsno").innerHTML = rezno;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 function page(number) {
@@ -105,28 +108,30 @@ function page(number) {
 
 function current_page() {
   var qs = new URLSearchParams(window.location.search);
-  var current = qs.get("start") / 10 + 1;
-  document.getElementById("page_" + current).setAttribute("class", "highlighted");
+  var start = parseInt(qs.get("start"), 10);
+  if (isNaN(start)) start = 0;
+  var current = (start / 10) + 1;
+  var el = document.getElementById("page_" + current);
+  if (el) {
+    el.setAttribute("class", "highlighted");
+  }
 }
 
 function showPages(max) {
   var qs = new URLSearchParams(window.location.search);
-  var start = qs.get("start");
-  var pagemax;
+  var start = parseInt(qs.get("start"), 10);
+  if (isNaN(start)) start = 0;
   start = Math.round(start / 10) + 1;
-  if (start == 1) {
-    start = 2;
-  }
-  pagemax = start + 2;
+  var pagemax = start + 2;
   if (pagemax > max) {
     pagemax = max + 1;
   }
   var xpages = "";
-  for (i = start - 1; i < pagemax; i++) {
-    xpages += "<span ><a href='' id='page_" + i + "'>[ " + i + " ]</a></span>";
+  for (var i = start - 1; i < pagemax; i++) {
+    xpages += "<span><a href='' id='page_" + i + "'>[ " + i + " ]</a></span>";
   }
   if (start < max - 1) {
-    xpages += "<span>...</span>" + "<span ><a href='' id='page_" + max + "'>[ " + max + " ]</a></span>";
+    xpages += "<span>...</span>" + "<span><a href='' id='page_" + max + "'>[ " + max + " ]</a></span>";
   }
 
   var pagesElement = document.getElementById("pages");
@@ -134,7 +139,7 @@ function showPages(max) {
     pagesElement.innerHTML = xpages;
   }
 
-  for (i = start - 1; i < pagemax; i++) {
+  for (var i = start - 1; i < pagemax; i++) {
     page(i);
   }
 
@@ -146,15 +151,17 @@ function getRandom() {
   var url_r = serverurl + '/v1/random/';
   fetch(url_r)
     .then(function(response) {
-      response.text().then(function(text) {
-        var ser_message = JSON.parse(text);
-        document.getElementById("job_title_r").innerHTML = ser_message.title;
-        document.getElementById("company_r").innerHTML = ser_message.company;
-        document.getElementById("location_r").innerHTML = (ser_message.location || []).join(', ') || ser_message.county || '';
-        document.getElementById("link_r").href = ser_message.url;
-      });
+      return response.json();
     })
-    .catch(err => console.log(err));
+    .then(function(ser_message) {
+      document.getElementById("job_title_r").innerHTML = escapeHtml(ser_message.title);
+      document.getElementById("company_r").innerHTML = escapeHtml(ser_message.company);
+      document.getElementById("location_r").innerHTML = escapeHtml((ser_message.location || []).join(', ') || ser_message.county || '');
+      document.getElementById("link_r").href = escapeHtml(ser_message.url);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 if (qisset()) {
@@ -172,6 +179,5 @@ if (qisset()) {
 } else {
   total(serverurl + "/v1/search/?q=*%3A*&rows=0");
   getRandom();
+  setInterval(getRandom, 5000);
 }
-
-setInterval(getRandom, 5000);
